3575ddf62dcfa0831b205a6026b199e4ccdf4228
Caren Keyes <cejkeyes@gmail.com>
2018-05-01 08:48:14 -0700

    add wrapper inside each section for formatting

--- public/client.js
+++ public/client.js
@@ -1,15 +1,15 @@
 function handleLogin(){
     $('.js-login-button').click(e =>{
         e.preventDefault();
-        $('.landing-page').addClass("hidden");
-        $('.event-page').removeClass("hidden");
+        $('.js-landing-page').addClass("hidden");
+        $('.js-event-page').removeClass("hidden");
     });
 };
 
 function handleActivity(){
-    $('.event-page').on('click', '.activity', e =>{
-        $('.event-page').addClass("hidden");
-        $('.activity-page').removeClass("hidden");
+    $('.js-event-page').on('click', '.activity', e =>{
+        $('.js-event-page').addClass("hidden");
+        $('.js-activity-page').removeClass("hidden");
     });
 };
 
