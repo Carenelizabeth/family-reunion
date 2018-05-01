99def335da280163186a29d5ad2abfcd20bdf2ff
Caren Keyes <cejkeyes@gmail.com>
2018-05-01 09:16:31 -0700

    add retrieve user input

--- public/client.js
+++ public/client.js
@@ -1,10 +1,29 @@
 function handleLogin(){
-    $('.js-login-button').click(e =>{
+    $('.js-login').submit(e =>{
         e.preventDefault();
-        $('.js-landing-page').addClass("hidden");
-        $('.js-event-page').removeClass("hidden");
+        let email = $(e.currentTarget).find('#login-email').val();
+        console.log(email);
+        let password = $(e.currentTarget).find('#user-password').val();
+        console.log(password);
+        let valid = validateLogin(email, password);
+        console.log(valid);
+        if(!(valid = true)){
+            alert("something went wrong")
+        }
+        else {
+            $('.js-landing-page').addClass("hidden");
+             $('.js-event-page').removeClass("hidden");};
     });
 };
+
+function validateLogin(email, password){
+    console.log(email);
+    if(!(email.includes('@'))){
+        alert("Please enter a valid email");
+        return false;
+    }
+
+}
 
 function handleActivity(){
     $('.js-event-page').on('click', '.activity', e =>{
