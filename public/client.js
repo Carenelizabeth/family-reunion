b4169f7d7c0b553737f343cf90d8c3033984d882
Caren Keyes <cejkeyes@gmail.com>
2018-05-01 17:13:40 -0700

    add function for displaying activities

--- public/client.js
+++ public/client.js
@@ -5,21 +5,35 @@ function handleLogin(){
         console.log(email);
         let password = $(e.currentTarget).find('#user-password').val();
         console.log(password);
-        let valid = validateLogin(email, password);
-        console.log(valid);
-        if(!(valid = true)){
-            alert("something went wrong")
-        }
-        else {
-            $('.js-landing-page').addClass("hidden");
-             $('.js-event-page').removeClass("hidden");};
+        validateLogin(email, password);
+        showEventPage();
     });
 };
+
+function showEventPage(){
+    $('.js-landing-page').addClass("hidden");
+    $('.js-event-page').removeClass("hidden");
+    const activity = activitySTORE.map((item, index) =>{
+        renderActivities(item);
+    });
+    console.log(activity);
+    $('.all-activities').html(activity);
+}
+
+function renderActivities(results){
+    console.log(results.activity_name);
+    return`        
+        <div class="activity">
+            <h2 class="activity-name">${results.activity_name}</h2>
+            <p class="host-name">${results.host_name}</p>
+            <p class="price">${results.activity_cost}</p>
+        </div>`;
+}
 
 function validateLogin(email, password){
     console.log(email);
     if(!(email.includes('@'))){
-        alert("Please enter a valid email");
+        alert("Please enter a valid email")
         return false;
     }
 
