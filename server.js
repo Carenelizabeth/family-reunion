e67f8d0cc144098629ca6bc01b51ae909c85dbcb
Caren Keyes <cejkeyes@gmail.com>
2018-05-03 15:32:37 -0700

    add runServer ad closeServer functions

--- server.js
+++ server.js
@@ -2,17 +2,54 @@
 
 const express = require('express');
 const morgan = require('morgan');
-const Mongoose = require('mongoose');
-Mongoose.Promise = global.Promise;
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 const app = express();
 
-const {PORT} = require('./config.js');
+const {DATABASE_URL, PORT} = require('./config.js');
 //app.listen(process.env.PORT || 8080);
 
 app.use(express.static('public'));
 app.use(morgan('common'));
 app.use(express.json());
 
-app.listen(PORT);
-module.exports = app
+function runServer(databaseUrl, port = PORT){
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err =>{
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () =>{
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+        
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+
+function closeServer(){
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) =>{
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.error(err));
+  }
+  
+  module.exports = { runServer, app, closeServer };
--- test/tests.js
+++ test/tests.js
@@ -2,11 +2,13 @@
 
 const chai = require('chai');
 const chaitHttp = require('chai-http');
+const faker = require('faker');
 const mongoose = require('mongoose');
 
 const expect = chai.expect;
 
-const app = require('../server.js');
+const {app, runServer, closeServer} = require('../server.js');
+const {TEST_DATABASE_URL} = require('../config')
 
 chai.use(chaitHttp);
 
