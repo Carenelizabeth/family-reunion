df5a357b2ff9e8d84b86156be6cafdb819103d82
Caren Keyes <cejkeyes@gmail.com>
2018-05-03 18:51:39 -0700

    add generate faker functions for event endpoint tests

--- test/tests.js
+++ test/tests.js
@@ -7,10 +7,34 @@ const mongoose = require('mongoose');
 
 const expect = chai.expect;
 
+const {Event} = require('../models')
 const {app, runServer, closeServer} = require('../server.js');
 const {TEST_DATABASE_URL} = require('../config')
 
 chai.use(chaitHttp);
+
+function seedEventData(){
+    console.info('seeding event data');
+    const eventData = []
+    for(let i=1; i<=10; i++){
+        eventData.push(generateEventData());
+    }
+    return Event.insertMany(eventData);
+}
+
+function generateEventData(){
+    return{
+        event_name: faker.random.words,
+        event_location: faker.random.locale,
+        event_dates: {start_date: faker.date.future, end_date: faker.date.future},
+        event_organizer: faker.firstName
+    }
+}
+
+function tearDownDb(){
+    console.warn('Deleting database');
+    return mongoose.connection.dropDatabase();
+}
 
 describe('Event planning API', function(){
     it('should answer a request to root with html', function(){
