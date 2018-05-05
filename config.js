7d3cbaebf14ae3a27baf4ed9a5969cf0f5323874
Caren Keyes <cejkeyes@gmail.com>
2018-05-04 17:23:20 -0700

    add mongodb service

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict'
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/family-reunion';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/family-reunion';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-family-reunion';
 exports.PORT = process.env.PORT || 8080;
