f867917422af219080370ba4270d6718fad68ebf
Caren Keyes <cejkeyes@gmail.com>
2018-05-09 11:36:18 -0700

    add schema for activity endpoint

--- models.js
+++ models.js
@@ -27,5 +27,36 @@ eventSchema.methods.serialize = function(){
     };
 };
 
+const costSchema = new mongoose.Schema({
+    adults: Number,
+    kids: Number,
+    group: Number,
+    group_size: Number
+})
+
+const activitySchema = new mongoose.Schema({
+    activity_name: {type: String, required: true},
+    activity_description: String,
+    activity_date: String,
+    activity_time: String,
+    activity_cost: costSchema,
+    activity_host: String,
+    attendees: [new mongoose.Schema({name: String})]
+})
+
+activitySchema.methods.serialize = function(){
+    return{
+        name: this.activity_name,
+        description: this.activity_description,
+        date: this.activity.date,
+        time: this.activity_time,
+        kid_cost: this.activity_cost.kids,
+        adult_cost: this.activity_cost.adults,
+        group_cost: this.activity_cost.groups,
+        group_size: this.activity_cost.group_size,
+        host: this.activity_host
+    }
+}
+
 const Event = mongoose.model('event', eventSchema);
 module.exports = {Event};
